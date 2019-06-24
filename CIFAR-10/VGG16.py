38ff8a594c7427536fa4e70e263ba627ffcba3f9
Programmer <hello>
2019-06-24 17:49:50 +0800

    VGG16 define

--- CIFAR-10/VGG16.py
+++ CIFAR-10/VGG16.py
@@ -3,10 +3,44 @@
 思考一下，效果为什么不好？
 """
 import tensorflow as tf
+import tensorflow.python.keras.layers as layers
 from tensorflow.python.keras.datasets.cifar10 import load_data
-from tensorflow.python.keras.applications import VGG16
 
-vgg16 = VGG16(include_top=False, input_shape=(32,32,3))
+# from tensorflow.python.keras.applications import VGG16
+# vgg16 = VGG16(include_top=False, input_shape=(32,32,3))
+
+# 等价实现
+vgg16 = tf.keras.Sequential([
+    layers.Conv2D(64, 3, padding='same', input_shape=(224, 224, 3), activation=tf.nn.relu),
+    layers.Conv2D(64, 3, padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(strides=2, padding='same'),
+
+    layers.Conv2D(128, 3, padding='same', activation=tf.nn.relu),
+    layers.Conv2D(128, 3, padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(strides=2, padding='same'),
+
+    layers.Conv2D(256, 3, padding='same', activation=tf.nn.relu),
+    layers.Conv2D(256, 3, padding='same', activation=tf.nn.relu),
+    layers.Conv2D(256, 3, padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(strides=2, padding='same'),
+
+    layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu),
+    layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu),
+    layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(strides=2, padding='same'),
+
+    layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu),
+    layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu),
+    layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(strides=2, padding='same'),
+
+    # layers.Flatten(),
+    # layers.Dense(4096, activation=tf.nn.relu),
+    # layers.Dense(4096, activation=tf.nn.relu),
+    # layers.Dense(1000, activation=tf.nn.softmax)
+])
+# vgg16.load_weights()
+
 
 (x_train, y_train), (x_test, y_test) = load_data()
 
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Tensorflow2-examples
-使用TensorFlow 2实现的项目集合
+使用TensorFlow 2实现的样例集合 (样例增加中)
 
 学习TF的资料很多，但相当一部分的程序实现基于过于老旧的TF版本，由于TensorFlow 2移除了大量的弃用API，旧的代码不再可用。
 
@@ -11,6 +11,9 @@ Project collection using TensorFlow 2
 There is a lot of information about learning TF, but a considerable part of the program implementation is based on the too old TF version. Since TensorFlow 2 removes a lot of deprecated APIs, the old code is no longer available.
 
 This project is built on TF 2.0 bata1 as a reference for your own learning projects and to help newcomers get started.
+
+### 新手建议的阅读顺序
+mnist-softmax -> minst-cnn -> cifar-10 -> cifar-VGG16
 
 ### 编译环境
 项目在TF-GPU 2.0 beta1上测试通过，其他相关包为最新正式版。
